50a583c67eba808660e53567698cce73726c12a5
Pawan M <pawanmurthy92@gmail.com>
2018-02-03 17:33:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return 'Happy holidays ${name}!'
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holday, name){
-  return `Happy ${holiday}, ${name}!'
+  return `Happy ${holiday}, ${name}!`
 }
